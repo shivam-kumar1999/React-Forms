32f5d260c0a6a7de8b0dff044dfe99f8aea21a51
Shivam kumar <shivamkumarhost@gmail.com>
2023-10-12 13:41:36 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,194 +1,191 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    firstName:"", lastName:"", email:"", country:"India",
+    streetAddress:"", city:"", state:"", postalCode:"",
+    comments:false, candidates:false, offers:false, pushNotifications:""
+  })
 
-  const [formData, setFormData]= useState({ firstName:"", lastName:"" ,email:"" , country:"India", 
-                                            streetAddress:"", city:"", state:"", postalCode:"",
-                                            comments:false, candidates:false, offers:false, 
-                                            pushNotifications:""})
-
-
-  function changeHandler(event){
-    const [{name, value, checked, type}]= event.target; 
-    
-    setFormData( prevState =>({...prevState , 
-      [event.target.name]:event.target.value}))
+  function changeHandler(event) {
+    const {name, value, checked, type} = event.target;
+    setFormData( (prev) => ({...prev, [name]:type === "checkbox" ? checked: value}) );
   }
 
+  function submitHandler(event) {
+    event.preventDefault();
 
-   return (
-    <div className="flex flex-col items-center justify-center mt-2 ">
+    console.log("Finally printing the value of Form Data:");
+    console.log(formData)
+  }
 
-      <form>
-            
-            <label htmlFor="firstName">First Name</label>  <br /> 
-            <input 
-                  type="text" 
-                  name="firstName" 
-                  id="firstName"  
-                  placeholder="Shivam"
-                  value={formData.firstName}
-                  onChange={changeHandler}
-                   className="border-2 "/>
+  return (
+   <div className="flex flex-col items-center mt-2">
+   <form onSubmit={submitHandler}>
 
-            
-            <br />
-            <label htmlFor="lastName">Last Name</label>  <br />  
-            <input 
-                  type="text" 
-                  name="lastName" 
-                  id="lastName"  
-                  placeholder="Kumar"
-                  value={formData.lastName}
-                  onChange={changeHandler}
-                   className="border-2 "/>   
+    <label htmlFor="firstName">First name</label>
+    <br/>
+    <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      placeholder="Shivam"
+      value={formData.firstName}
+      onChange={changeHandler}
+      className="outline"
+    />
 
+    <br/>
+    <label htmlFor="lastName">Last name</label>
+    <br/>
+    <input
+      type="text"
+      name="lastName"
+      id="lastName"
+      placeholder="Kumar"
+      value={formData.lastName}
+      onChange={changeHandler}
+      className="outline"
+    />
 
-            <br />
-            <label htmlFor="email">Email </label>  <br /> 
-            <input 
-                  type="email" 
-                  name="email" 
-                  id="email"  
-                  placeholder="name@gmail.com"
-                  value={formData.email}
-                  onChange={changeHandler}
-                   className="border-2 "/>  
+    <br/>
+    <label htmlFor="email">Email Address</label>
+    <br/>
+    <input
+      type="email"
+      name="email"
+      id="email"
+      placeholder="shivam@abcd.com"
+      value={formData.email}
+      onChange={changeHandler}
+      className="outline"
+    />
 
+    <br/>
+    <label htmlFor="country">Country</label>
+    <br/>
+    <select
+      id="country"
+      name="country"
+      value={formData.country}
+      onChange={changeHandler}
+      className="outline"
+      >
 
-              <br />     
-             <label htmlFor="country">Country</label> <br />
-             <select name="country" id="country" value={formData.country}>
+      <option>India</option>
+      <option>United States</option>
+      <option>Canada</option>
+      <option>Mexico</option>
+    </select>
 
-                <option value="">India</option>
-                <option value="">Nepal</option>
-                <option value="">Bhutan</option>
-                <option value="">Srilanka</option>
-                <option value="">China</option>
-                <option value="">Pakistan</option>
-               
-              </select>
+    <br/>
+    <label htmlFor="streetAddress">Street Address</label>
+    <br/>
+    <input
+      type="text"
+      name="streetAddress"
+      id="streetAddress"
+      placeholder="B-25C"
+      value={formData.streetAddress}
+      onChange={changeHandler}
+      className="outline"
+    />
 
+    <br/>
+    <label htmlFor="city">City</label>
+    <br/>
+    <input
+      type="text"
+      name="city"
+      id="city"
+      placeholder="B-25C"
+      value={formData.city}
+      onChange={changeHandler}
+      className="outline"
+    />
 
+  <br/>
+    <label htmlFor="state">State / Province</label>
+    <br/>
+    <input
+      type="text"
+      name="state"
+      id="state"
+      placeholder="Bihar"
+      value={formData.state}
+      onChange={changeHandler}
+      className="outline"
+    />
 
-               <br/>
-                <label htmlFor="streetAddress">Street Address</label>
-                <br/>
-                <input
-                  type="text"
-                  name="streetAddress"
-                  id="streetAddress"
-                  placeholder="B-25C"
-                  value={formData.streetAddress}
-                  onChange={changeHandler}
-                  className="outline"
-                />
+    <br/>
+    <label htmlFor="postalCode">Postal Code</label>
+    <br/>
+    <input
+      type="text"
+      name="postalCode"
+      id="postalCode"
+      placeholder="110077"
+      value={formData.postalCode}
+      onChange={changeHandler}
+      className="outline"
+    />
 
+    <br/>
+    <br/>
+    <fieldset>
+      <legend>By Email</legend>
 
+      <div className="flex">
+      <input
+        id="comments"
+        name="comments"
+        type="checkbox"
+        checked={formData.comments}
+        onChange={changeHandler}
+      />
+      <div>
+        <label htmlFor="comments">Comments</label>
+        <p>Get notified when someones posts a comment on a posting.</p>
+      </div>
+      </div>
 
-                <br/>
-                <label htmlFor="city">City</label>
-                <br/>
-                <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  placeholder="B-25C"
-                  value={formData.city}
-                  onChange={changeHandler}
-                  className="outline"
-                />
+      <div className="flex">
+      <input
+        id="candidates"
+        name="candidates"
+        type="checkbox"
+        checked={formData.candidates}
+        onChange={changeHandler}
+      />
+      <div>
+        <label htmlFor="candidates">Candidates</label>
+        <p>Get notified when a candidate applies for a job.</p>
+      </div>
+      </div>
 
+      <div className="flex">
+      <input
+        id="offers"
+        name="offers"
+        type="checkbox"
+        checked={formData.offers}
+        onChange={changeHandler}
+      />
+      <div>
+        <label htmlFor="offers">Offers</label>
+        <p>Get notified when a candidate accepts or rejects an offer.
 
-               <br/>
-               <label htmlFor="state">State / Province</label>
-               <br/>
-               <input
-                 type="text"
-                 name="state"
-                 id="state"
-                 placeholder="Bihar"
-                 value={formData.state}
-                 onChange={changeHandler}
-                 className="outline"
-               />
-
-
-               <br/>
-              <label htmlFor="postalCode">Postal Code</label>
-              <br/>
-              <input
-                type="text"
-                name="postalCode"
-                id="postalCode"
-                placeholder="110077"
-                value={formData.postalCode}
-                onChange={changeHandler}
-                className="outline"
-              />
-
-              <br/>
-              <br/>
-              <fieldset>
-                <legend>By Email</legend>
-
-                <div className="flex">
-                <input
-                  id="comments"
-                  name="comments"
-                  type="checkbox"
-                  checked={formData.comments}
-                  onChange={changeHandler}
-                />
-                <div>
-                  <label htmlFor="comments">Comments</label>
-                  <p>Get notified when someones posts a comment on a posting.</p>
-                </div>
-                </div>
-
-
-
-                  <div className="flex">
-                  <input
-                    id="candidates"
-                    name="candidates"
-                    type="checkbox"
-                    checked={formData.candidates}
-                    onChange={changeHandler}
-                  />
-
-                 <div>
-                   <label htmlFor="candidates">Candidates</label>
-                   <p>Get notified when a candidate applies for a job.</p>
-                 </div>
-                 </div>
-
-                 <div className="flex">
-                 <input
-                   id="offers"
-                   name="offers"
-                              type="checkbox"
-                   checked={formData.offers}
-                   onChange={changeHandler}
-                 />
-
-
-                 <div>
-
-                 <label htmlFor="offers">Offers</label>
-                 <p>Get notified when a candidate accepts or rejects an offer</p>
-
-              </div>
-              </div>
+</p>
+      </div>
+      </div>
       
 
 
-               </fieldset>
-            
+    </fieldset>
 
-               <br/>
+    <br/>
     <br/>
     <fieldset>
       <legend>Push Notifications</legend>
@@ -239,13 +236,10 @@ function App() {
 
 
 
+   </form>
 
-
-
-      </form>
-
-    </div>
-  )
+   </div>
+  );
 }
 
-export default App
+export default App;
